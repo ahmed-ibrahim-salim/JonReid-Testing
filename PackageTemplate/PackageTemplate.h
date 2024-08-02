fdf498b4f767a53c32fd85373154c923b4a86e62
Ahmed Ibrahim <ahmed.ibrahim@efreshli.com>
2024-08-02 11:02:03 +0300

    initial

---
+++ PackageTemplate/PackageTemplate.h
@@ -0,0 +1,18 @@
+//
+//  PackageTemplate.h
+//  PackageTemplate
+//
+//  Created by Ahmed Ibrahim on 02/08/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PackageTemplate.
+FOUNDATION_EXPORT double PackageTemplateVersionNumber;
+
+//! Project version string for PackageTemplate.
+FOUNDATION_EXPORT const unsigned char PackageTemplateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PackageTemplate/PublicHeader.h>
+
+
---
+++ PackageTemplate/Sources/TestingNetworkRequests.swift
@@ -0,0 +1,8 @@
+//
+//  TestingNetworkRequests.swift
+//  PackageTemplate
+//
+//  Created by Ahmed Ibrahim on 02/08/2024.
+//
+
+import Foundation
